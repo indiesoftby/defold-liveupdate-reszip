ed0b5e790c0a7157210b4e49b84c8df72b90bae2
Artsiom Trubchyk <aglitchman@gmail.com>
2021-02-25 14:13:20 +0300

    Add ZIP archive extraction and loading

---
+++ liveupdate_reszip/reszip.lua
@@ -0,0 +1,49 @@
+local M = {}
+
+local function call_callback_and_cleanup(self, err)
+    M._resources_zip = nil
+    M._missing_resources = nil
+    M._callback(self, err)
+    M._callback = nil
+end
+
+local function store_missing_resource_from_zip(self, hexdigest, status)
+    if status then
+        if next(M._missing_resources) ~= nil then
+            local res_hash = table.remove(M._missing_resources)
+
+            local data = liveupdate_miniz.extract_file(M._resources_zip, res_hash)
+            if data then
+                resource.store_resource(resource.get_current_manifest(), data, res_hash, store_missing_resource_from_zip)
+            else
+                call_callback_and_cleanup(self, "Can't extract file " .. res_hash)
+            end
+        else
+            -- SUCCESS!
+            call_callback_and_cleanup(self, nil)
+        end
+    else
+        call_callback_and_cleanup(self, "Error happened while storing resource: " .. hexdigest)
+    end
+end
+
+local function http_request_handler(self, id, response)
+    if (response.status == 200 or response.status == 304) and response.error == nil then
+        print("DEBUG: Resources ZIP loaded, opening...")
+        M._resources_zip = response.response
+        if liveupdate_miniz.validate_zip(M._resources_zip) then
+            store_missing_resource_from_zip(self, nil, true)
+        else
+            call_callback_and_cleanup(self, "Invalid format of the ZIP file")
+        end
+    else
+        call_callback_and_cleanup(self, "Error happened while downloading:" .. response.status)
+    end
+end
+
+function M.request_and_load_zip(filename, missing_resources, callback)
+    M._callback = callback
+    M._missing_resources = missing_resources
+
+    http.request(filename, "GET", http_request_handler)
+end
---
+++ liveupdate_reszip/src/miniz_ext.cpp
@@ -0,0 +1,85 @@
+#define LIB_NAME "liveupdate_miniz"
+#define MODULE_NAME "liveupdate_miniz"
+
+#include "miniz.h"
+
+#include <dmsdk/sdk.h>
+#include <string.h>
+
+static int ExtractFile(lua_State *L) {
+    size_t buf_len = 0;
+    const char *buf = luaL_checklstring(L, 1, &buf_len);
+    const char *archive_filename = luaL_checkstring(L, 2);
+
+    mz_zip_archive zip_archive;
+    memset(&zip_archive, 0, sizeof(zip_archive));
+
+    if (!mz_zip_reader_init_mem(&zip_archive, buf, buf_len, 0)) {
+        dmLogError("mz_zip_reader_init_mem() failed!");
+
+        lua_pushnil(L);
+        return 1;
+    }
+
+    size_t uncomp_size;
+    void *p = mz_zip_reader_extract_file_to_heap(&zip_archive, archive_filename, &uncomp_size, 0);
+    if (!p) {
+        dmLogError("mz_zip_reader_extract_file_to_heap() failed!");
+
+        mz_zip_reader_end(&zip_archive);
+        lua_pushnil(L);
+        return 1;
+    }
+
+    lua_pushlstring(L, (char *)p, uncomp_size);
+
+    mz_free(p);
+    mz_zip_reader_end(&zip_archive);
+
+    return 1;
+}
+
+static int ValidateZip(lua_State *L) {
+    size_t buf_len = 0;
+    const char *buf = luaL_checklstring(L, 1, &buf_len);
+
+    mz_zip_archive zip_archive;
+    memset(&zip_archive, 0, sizeof(zip_archive));
+
+    if (!mz_zip_reader_init_mem(&zip_archive, buf, buf_len, 0)) {
+        dmLogError("mz_zip_reader_init_mem() failed!");
+
+        lua_pushboolean(L, 0);
+        return 1;
+    }
+
+    mz_zip_reader_end(&zip_archive);
+
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
+// Functions exposed to Lua
+static const luaL_reg Module_methods[] = {{"extract_file", ExtractFile},
+                                          {"validate_zip", ValidateZip},
+                                          /* Sentinel: */
+                                          {NULL, NULL}};
+
+static void LuaInit(lua_State *L) {
+    int top = lua_gettop(L);
+
+    // Register lua names
+    luaL_register(L, MODULE_NAME, Module_methods);
+
+    lua_pop(L, 1);
+    assert(top == lua_gettop(L));
+}
+
+static dmExtension::Result InitializeExt(dmExtension::Params *params) {
+    LuaInit(params->m_L);
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeExt(dmExtension::Params *params) { return dmExtension::RESULT_OK; }
+
+DM_DECLARE_EXTENSION(liveupdate_miniz, LIB_NAME, 0, 0, InitializeExt, 0, 0, FinalizeExt)
