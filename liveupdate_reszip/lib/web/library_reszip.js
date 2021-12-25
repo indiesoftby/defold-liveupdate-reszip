1203845dfc7913e010e2b9cf8f86ca6825399378
Artsiom Trubchyk <aglitchman@gmail.com>
2021-12-25 20:32:18 +0300

    Add file preloading for HTML5

--- liveupdate_reszip/lib/web/library_reszip.js
+++ liveupdate_reszip/lib/web/library_reszip.js
@@ -1,26 +1,90 @@
 var LibraryResZip = {
-    $ResZip: {},
+    $ResZip: {
+        //
+        _preloads: {},
+    },
 
     dmResZipRequestFileAsync: function (url, context, onprogress, onerror, onload) {
-        FileLoader.load(
-            UTF8ToString(url),
-            "arraybuffer",
-            0,
-            function (loaded, total) {
+        var callbacks = {
+            onprogress: function (loaded, total) {
                 {{{ makeDynCall('viii', 'onprogress') }}}(context, loaded, total);
             },
-            function (err) {
+            onerror: function (err) {
                 var pError = allocate(intArrayFromString(err), "i8", ALLOC_NORMAL);
                 {{{ makeDynCall('vii', 'onerror') }}}(context, pError);
                 _free(pError);
             },
-            function (response) {
+            onload: function (response) {
                 var ab = new Uint8Array(response);
                 var b = allocate(ab, "i8", ALLOC_NORMAL);
                 {{{ makeDynCall('viii', 'onload') }}}(context, b, ab.length);
                 _free(b);
-            }
-        );
+            },
+        };
+
+        if (context === undefined) {
+            //
+            // Undefined context means that the function is called from the JS.
+            //
+            // Let's start loading the url and keep the progress in the `_preloads` object.
+            //
+
+            var preload = {
+                events: {},
+                handler: function () {
+                    var args = Array.prototype.slice.call(arguments);
+                    var name = args.shift();
+                    preload.events[name] = args;
+                },
+            };
+            ResZip._preloads[url] = preload;
+
+            FileLoader.load(
+                url,
+                "arraybuffer",
+                0,
+                function (loaded, total) {
+                    preload.handler("onprogress", loaded, total);
+                },
+                function (err) {
+                    preload.handler("onerror", err);
+                },
+                function (response) {
+                    preload.handler("onload", response);
+                }
+            );
+            return;
+        }
+
+        // Convert C/C++ char* to a JavaScript string
+        url = UTF8ToString(url);
+
+        // Check if data has been already preloaded
+        if (ResZip._preloads[url]) {
+            //
+            // Preloading of this URL in progress or finished
+            //
+            var preload = ResZip._preloads[url];
+            delete ResZip._preloads[url];
+
+            preload.handler = function () {
+                var args = Array.prototype.slice.call(arguments);
+                var name = args.shift();
+                callbacks[name].apply(null, args);
+            };
+
+            ["onprogress", "onerror", "onload"].forEach(function (name) {
+                var args = preload.events[name];
+                if (args !== undefined) {
+                    callbacks[name].apply(null, args);
+                }
+            });
+        } else {
+            //
+            // Normal usage - load something from URL
+            //
+            FileLoader.load(url, "arraybuffer", 0, callbacks.onprogress, callbacks.onerror, callbacks.onload);
+        }
     },
 };
 
