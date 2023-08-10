2ec413536aee91a9b406c1241eb9a149cd3a49f9
Artsiom Trubchyk <aglitchman@gmail.com>
2023-08-10 17:42:38 +0300

    WIP Use add_mount

--- dev_build_web.sh
+++ dev_build_web.sh
@@ -7,10 +7,11 @@
 
 set -e
 
-PLATFORM=js-web
+PLATFORM=wasm-web
 
 rm -rf build
 mkdir -p build/public
-java -jar bob.jar --email f@b.com --auth 123 --texture-compression true --bundle-output build/bundle/${PLATFORM} --build-report-html build/public/build_report_latest.html --platform ${PLATFORM} --archive --liveupdate yes --variant debug resolve build bundle
+java -jar bob.jar --email f@b.com --auth 123 --texture-compression true --bundle-output build/bundle/${PLATFORM} --build-report-html build/public/build_report_latest.html --platform ${PLATFORM} --architectures ${PLATFORM} --archive --liveupdate yes --variant debug resolve build bundle
 mv build/liveupdate_output/*.zip build/bundle/${PLATFORM}/liveupdate_reszip_demo/resources.zip
-(cd build/bundle/${PLATFORM}/liveupdate_reszip_demo/ && http-server -c-)
+# (cd build/bundle/${PLATFORM}/liveupdate_reszip_demo/ && http-server -c-)
+http-server -c-
