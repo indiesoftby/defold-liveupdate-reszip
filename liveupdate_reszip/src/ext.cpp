408168bfae43d810c69f5481fede04d29510ec4e
Artsiom Trubchyk <aglitchman@gmail.com>
2023-10-31 15:15:56 +0300

    Remove miniz

--- liveupdate_reszip/src/ext.cpp
+++ liveupdate_reszip/src/ext.cpp
@@ -1,105 +1,7 @@
 #define LIB_NAME "liveupdate_reszip_ext"
 #define MODULE_NAME "liveupdate_reszip_ext"
 
-#include "miniz.h"
-
 #include <dmsdk/sdk.h>
-#include <stdint.h>
-#include <string.h>
-
-static int ExtractFile(lua_State *L) {
-    size_t buf_len = 0;
-    const char *buf = luaL_checklstring(L, 1, &buf_len);
-    const char *archive_filename = luaL_checkstring(L, 2);
-
-    mz_zip_archive zip_archive;
-    memset(&zip_archive, 0, sizeof(zip_archive));
-
-    if (!mz_zip_reader_init_mem(&zip_archive, buf, buf_len, 0)) {
-        dmLogError("mz_zip_reader_init_mem() failed!");
-
-        lua_pushnil(L);
-        return 1;
-    }
-
-    size_t uncomp_size;
-    void *p = mz_zip_reader_extract_file_to_heap(&zip_archive, archive_filename, &uncomp_size, 0);
-    if (!p) {
-        dmLogError("mz_zip_reader_extract_file_to_heap() failed!");
-
-        mz_zip_reader_end(&zip_archive);
-        lua_pushnil(L);
-        return 1;
-    }
-
-    lua_pushlstring(L, (char *)p, uncomp_size);
-
-    mz_free(p);
-    mz_zip_reader_end(&zip_archive);
-
-    return 1;
-}
-
-static int ValidateZip(lua_State *L) {
-    size_t buf_len = 0;
-    const char *buf = luaL_checklstring(L, 1, &buf_len);
-
-    mz_zip_archive zip_archive;
-    memset(&zip_archive, 0, sizeof(zip_archive));
-
-    if (!mz_zip_reader_init_mem(&zip_archive, buf, buf_len, 0)) {
-        dmLogError("mz_zip_reader_init_mem() failed!");
-
-        lua_pushboolean(L, 0);
-        return 1;
-    }
-
-    mz_zip_reader_end(&zip_archive);
-
-    lua_pushboolean(L, 1);
-    return 1;
-}
-
-static int ListResources(lua_State *L) {
-    size_t buf_len = 0;
-    const char *buf = luaL_checklstring(L, 1, &buf_len);
-
-    mz_zip_archive zip_archive;
-    memset(&zip_archive, 0, sizeof(zip_archive));
-
-    if (!mz_zip_reader_init_mem(&zip_archive, buf, buf_len, 0)) {
-        dmLogError("mz_zip_reader_init_mem() failed!");
-
-        lua_pushnil(L);
-        return 1;
-    }
-
-    uint32_t file_count = mz_zip_reader_get_num_files(&zip_archive);
-    lua_createtable(L, file_count, 0);
-
-    if (file_count > 0) {
-        uint32_t fi = 1;
-        for (uint32_t i = 0; i < file_count; i++) {
-            mz_zip_archive_file_stat file_stat;
-            if (!mz_zip_reader_file_stat(&zip_archive, i, &file_stat))
-                continue;
-            if (mz_zip_reader_is_file_a_directory(&zip_archive, i))
-                continue; // skip directories
-            if (strlen(file_stat.m_filename) != 40)
-                continue; // not a resource
-
-            lua_pushnumber(L, fi);
-            lua_pushstring(L, file_stat.m_filename);
-            lua_settable(L, -3);
-
-            fi++;
-        }
-    }
-
-    mz_zip_reader_end(&zip_archive);
-
-    return 1;
-}
 
 #if defined(DM_PLATFORM_HTML5)
 
@@ -210,20 +112,11 @@ static int RequestFile(lua_State *L) {
 
 #endif
 
-static int UpdateJobQueue(lua_State *L)
-{
-    dmLogInfo("`update_job_queue` is deprecated and does nothing.");
-    return 0;
-}
-
 // Functions exposed to Lua
-static const luaL_reg Module_methods[] = {{"extract_file", ExtractFile},
-                                          {"validate_zip", ValidateZip},
-                                          {"list_resources", ListResources},
+static const luaL_reg Module_methods[] = {
 #if defined(DM_PLATFORM_HTML5)
                                           {"request_file", RequestFile},
 #endif
-                                          {"update_job_queue", UpdateJobQueue},
                                           /* Sentinel: */
                                           {NULL, NULL}};
 
